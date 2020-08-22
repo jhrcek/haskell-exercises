249753b849ed03daad2325f3b185cb8516e99dec
Jan Hrček <honza.hrk@gmail.com>
2020-08-22 12:47:28 +0200

    Partially complete 06-TypeFamilies

--- 06-TypeFamilies/src/Exercises.hs
+++ 06-TypeFamilies/src/Exercises.hs
@@ -1,7 +1,8 @@
-{-# LANGUAGE DataKinds     #-}
-{-# LANGUAGE GADTs         #-}
-{-# LANGUAGE TypeFamilies  #-}
-{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE DataKinds            #-}
+{-# LANGUAGE GADTs                #-}
+{-# LANGUAGE TypeFamilies         #-}
+{-# LANGUAGE TypeOperators        #-}
+{-# LANGUAGE UndecidableInstances #-}
 module Exercises where
 
 import Data.Kind (Constraint, Type)
@@ -22,8 +23,15 @@ data Nat = Z | S Nat
 -- | a. Use the @TypeOperators@ extension to rewrite the 'Add' family with the
 -- name '+':
 
+type family (+) (x :: Nat) (y :: Nat) :: Nat where
+  'Z     + y = y
+  ('S x) + y = 'S (x + y)
+
 -- | b. Write a type family '**' that multiplies two naturals using '(+)'. Which
 -- extension are you being told to enable? Why?
+type family (**) (x :: Nat) (y :: Nat) :: Nat where
+  'Z **     y = 'Z
+  ('S x) ** y = y + (x ** y)
 
 data SNat (value :: Nat) where
   SZ :: SNat 'Z
@@ -31,6 +39,9 @@ data SNat (value :: Nat) where
 
 -- | c. Write a function to add two 'SNat' values.
 
+addSNat :: SNat a -> SNat b -> SNat (a+b)
+addSNat SZ b     = b
+addSNat (SS a) b = SS (addSNat a b)
 
 
 
@@ -44,14 +55,17 @@ data Vector (count :: Nat) (a :: Type) where
 -- | a. Write a function that appends two vectors together. What would the size
 -- of the result be?
 
--- append :: Vector m a -> Vector n a -> Vector ??? a
+append :: Vector m a -> Vector n a -> Vector (m+n) a
+append VNil ys         = ys
+append (VCons x xs) ys = VCons x (append xs ys)
 
 -- | b. Write a 'flatMap' function that takes a @Vector n a@, and a function
 -- @a -> Vector m b@, and produces a list that is the concatenation of these
 -- results. This could end up being a deceptively big job.
 
--- flatMap :: Vector n a -> (a -> Vector m b) -> Vector ??? b
-flatMap = error "Implement me!"
+flatMap :: Vector n a -> (a -> Vector m b) -> Vector (n**m) b
+flatMap VNil _         = VNil
+flatMap (VCons x xs) f = f x `append` flatMap xs f
 
 
 
@@ -60,11 +74,19 @@ flatMap = error "Implement me!"
 {- THREE -}
 
 -- | a. More boolean fun! Write the type-level @&&@ function for booleans.
-
+type family (&&) (a :: Bool) (b :: Bool) :: Bool where
+   True && a  = a
+   False && b = False
 -- | b. Write the type-level @||@ function for booleans.
-
+type family (||) (a :: Bool) (b :: Bool) :: Bool where
+   True || a  = True
+   False || b = b
 -- | c. Write an 'All' function that returns @'True@ if all the values in a
 -- type-level list of boleans are @'True@.
+type family All  (as::[Bool]) :: Bool where
+   All '[] = True
+   All (True ':as) = All as
+   All (False ':bs) = False
 
 
 
@@ -74,12 +96,23 @@ flatMap = error "Implement me!"
 
 -- | a. Nat fun! Write a type-level 'compare' function using the promoted
 -- 'Ordering' type.
+type family Compare (a::Nat) (b::Nat) :: Ordering where
+  Compare Z Z = EQ
+  Compare Z b = LT
+  Compare b Z = GT
+  Compare (S x) (S y) = Compare x y
 
 -- | b. Write a 'Max' family to get the maximum of two natural numbers.
+type family Max (a::Nat) (b::Nat) :: Nat where
+  Max a Z = a
+  Max Z b = b
+  Max (S a) (S b) = S (Max a b)
 
 -- | c. Write a family to get the maximum natural in a list.
-
-
+type family Maximum (as :: [Nat]) :: Nat where
+  Maximum '[] = Z
+  Maximum '[a] = a
+  Maximum (a ': as) = Max a (Maximum as)
 
 
 
@@ -88,18 +121,76 @@ flatMap = error "Implement me!"
 data Tree = Empty | Node Tree Nat Tree
 
 -- | Write a type family to insert a promoted 'Nat' into a promoted 'Tree'.
+type family InsTree (n :: Nat) (t :: Tree) :: Tree where
+  InsTree n Empty = Node Empty n Empty
+  InsTree n (Node a m b) = InsHelp (Compare n m) n (Node a m b)
 
-
-
-
+type family InsHelp (o :: Ordering) (n :: Nat) (t :: Tree) :: Tree where
+  InsHelp LT n (Node a m b) = Node (InsTree n a) m b
+  InsHelp EQ n (Node a m b) = Node a m b
+  InsHelp GT n (Node a m b) = Node a m (InsTree n b)
 
 {- SIX -}
 
 -- | Write a type family to /delete/ a promoted 'Nat' from a promoted 'Tree'.
 
+-- type family DelTree (n::Nat) (t :: Tree) :: Tree where
+--   DelTree n Empty = Empty
+--   DelTree n (Node l x r) = DelHelp (Compare n x) n (Node l x r)
 
+-- type family DelHelp (o::Ordering) (n::Nat) (t :: Tree) :: Tree where
+--   DelHelp LT n (Node l x r) = Node (DelTree n l) x r
+--   DelHelp GT n (Node l x r) = Node l x (DelTree n r)
+--   DelHelp EQ n (Node Empty x r) = r
+--   DelHelp EQ n (Node l x Empty) = l
+--   DelHelp EQ n (Node l x r) = l -- TODO need to find the largest elem in l, put it in place of x and fix potentially broken l
 
+-- SUPER UGLY.
+type family Delete (x :: Nat) (xs :: Tree) :: Tree where
+  Delete x  'Empty       = 'Empty
+  Delete x ('Node l c r) = Delete' (Compare x c) x ('Node l c r)
 
+-- We can't let-bind the result of a function like 'Compare', so we have to
+-- have a helper family to compute the above.
+type family Delete' (o :: Ordering) (x :: Nat) (xs :: Tree) :: Tree where
+  Delete' 'LT x ('Node  l     c r) = 'Node (Delete x l) c r
+  Delete' 'GT x ('Node  l     c r) = 'Node l c (Delete x r)
+  Delete' 'EQ x ('Node 'Empty c r) = r
+  Delete' 'EQ x ('Node  l     c r) = Repair (Biggest l) r
+
+-- ... We also can't have a helper family for the last case above, so we need
+-- two more helper families:
+type family Repair (parts :: (Nat, Tree)) (xs :: Tree) :: Tree where
+  Repair '(c, l) r = 'Node l c r
+
+type family Biggest (xs :: Tree) :: (Nat, Tree) where
+  Biggest ('Node l c 'Empty) = '(c, l)
+  Biggest ('Node l c r)      = Biggest' l c (Biggest r)
+
+-- Reconstructing the tree would also require a let-binding, so we have
+-- /another/ helper family. Eurgh!
+type family Biggest' (l :: Tree) (c :: Nat) (r' :: (Nat, Tree)) :: (Nat, Tree) where
+  Biggest' l c '(x, r) = '(x, 'Node l c r)
+
+-- We can use this type to write "tests" for the above. Any mention of Refl
+-- will force GHC to try to unify the two type parameters. If it fails, we get
+-- a type error!
+data (x :: Tree) :~: (y :: Tree) where
+  Refl :: x :~: x
+
+deleteTest0 :: Delete 'Z 'Empty :~: 'Empty
+deleteTest0 = Refl
+
+deleteTest1 :: Delete 'Z (InsTree 'Z 'Empty) :~: 'Empty
+deleteTest1 = Refl
+
+deleteTest2 :: InsTree 'Z (InsTree 'Z 'Empty) :~: InsTree 'Z 'Empty
+deleteTest2 = Refl
+
+deleteTest3
+   :: InsTree ('S 'Z) (InsTree 'Z 'Empty)
+  :~: 'Node 'Empty 'Z ('Node 'Empty ('S 'Z) 'Empty)
+deleteTest3 = Refl
 
 {- SEVEN -}
 
@@ -111,7 +202,13 @@ data HList (xs :: [Type]) where
   HCons :: x -> HList xs -> HList (x ': xs)
 
 -- | Write a function that appends two 'HList's.
+type family (++) (xs :: [Type]) (ys :: [Type]) :: [Type] where
+  '[] ++ ys = ys
+  (x ': xs) ++ ys = x ': (xs ++ ys)
 
+appHLs :: HList xs -> HList ys -> HList (xs ++ ys)
+appHLs HNil ys         = ys
+appHLs (HCons x xs) ys = HCons x (appHLs xs ys)
 
 
 
@@ -135,13 +232,28 @@ type family CAppend (x :: Constraint) (y :: Constraint) :: Constraint where
 -- list of types, and builds a constraint on all the types.
 
 type family Every (c :: Type -> Constraint) (x :: [Type]) :: Constraint where
-  -- ...
+  Every c '[] = ()
+  Every c (x ': xs) = (c x, Every c xs)
 
 -- | b. Write a 'Show' instance for 'HList' that requires a 'Show' instance for
 -- every type in the list.
+instance Every Show xs => Show (HList xs) where
+  show HNil         = "HNil"
+  show (HCons x xs) = "HCons " <> show x <> " " <> show xs
 
 -- | c. Write an 'Eq' instance for 'HList'. Then, write an 'Ord' instance.
 -- Was this expected behaviour? Why did we need the constraints?
+instance Every Eq xs => Eq (HList xs) where
+  HNil == HNil = True
+  HCons x xs == HCons y ys = x == y && xs == ys
+
+instance (Every Eq xs, Every Ord xs) => Ord (HList xs) where
+  compare HNil HNil = EQ
+  compare (HCons x xs) (HCons y ys) = case compare x y of
+    EQ -> compare xs ys
+    LT -> LT
+    GT -> GT
+
 
 
 
@@ -151,6 +263,25 @@ type family Every (c :: Type -> Constraint) (x :: [Type]) :: Constraint where
 
 -- | a. Write a type family to calculate all natural numbers up to a given
 -- input natural.
+type family NatsUpTo (n :: Nat) :: [Nat] where
+  NatsUpTo n = Reverse (CountDown n)
+
+type family CountDown (n :: Nat) :: [Nat] where
+  CountDown Z = '[Z]
+  CountDown (S n) = S n ': CountDown n
+
+type family Reverse (xs :: [Nat]) :: [Nat] where
+  Reverse xs = RevHelp xs '[]
+
+type family RevHelp (xs :: [Nat]) (ys :: [Nat]) :: [Nat] where
+  RevHelp '[] ys = ys
+  RevHelp (x ': xs) ys = RevHelp xs (x ': ys)
+
+data (x :: [Nat]) :~~~: (y :: [Nat]) where
+  Refle :: x :~~~: x
+
+upToTest :: NatsUpTo (S (S Z)) :~~~: '[Z, S Z, S (S Z)]
+upToTest = Refle
 
 -- | b. Write a type-level prime number sieve.
 
